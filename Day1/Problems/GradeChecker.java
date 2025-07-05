cbc972460441389998dc25ff992221218aff89a2
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-05 12:51:18 +0800

    Update GradeChecker.java

--- Day1/Problems/GradeChecker.java
+++ Day1/Problems/GradeChecker.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter grade: ");
+        int grade = input.nextInt();
+
+        if (grade >= 75 && grade <= 80) {
+            System.out.print("E");
+        } else if (grade >= 81 && grade <= 84) {
+            System.out.print("C");
+        } else if (grade >= 85 && grade <= 89) {
+            System.out.print("B");
+        } else if (grade >= 90 && grade <= 100) {
+            System.out.print("A");
+        } else {
+            System.out.print("F");
+        }
+    }
+}
