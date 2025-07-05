05c2fa6e01af826d96b9adc4f0848c6fb17fe45c
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-05 20:39:30 +0800

    Update SumDifferenceProduct.java

--- Day1/Problems/SumDifferenceProduct.java
+++ Day1/Problems/SumDifferenceProduct.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter b: ");
+        int b = input.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+
+    }
+}
