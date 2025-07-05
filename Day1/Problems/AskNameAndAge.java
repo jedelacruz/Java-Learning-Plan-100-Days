9819597c5bd72da14d362f3c1b04d9742cfbeab2
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-05 12:25:21 +0800

    Update AskNameAndAge.java

--- Day1/Problems/AskNameAndAge.java
+++ Day1/Problems/AskNameAndAge.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = input.nextInt();
+
+        System.out.println("Hello " + name + ", You are " + age + " years old.");
+
+    }
+}
