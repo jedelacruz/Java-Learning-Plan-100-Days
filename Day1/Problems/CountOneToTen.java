68fdb68803c51b004b1d32fcf64562241c9693eb
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-05 12:47:26 +0800

    Update CountOneToTen.java

--- Day1/Problems/CountOneToTen.java
+++ Day1/Problems/CountOneToTen.java
@@ -1,1 +1,14 @@
+public class Main {
+    public static void main(String[] agrs){
 
+        for(int i = 1; i < 11; i++){
+            System.out.println(i);
+        }
+        int i = 1;
+        while(i < 11){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
