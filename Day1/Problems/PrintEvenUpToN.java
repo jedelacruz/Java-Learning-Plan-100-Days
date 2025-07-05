908a739094bfb28919658bfc9620e529ed4180ae
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-05 12:53:31 +0800

    Update PrintEvenUpToN.java

--- Day1/Problems/PrintEvenUpToN.java
+++ Day1/Problems/PrintEvenUpToN.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = input.nextInt();
+
+        for(int i = 0; i <= n; i++){
+            if(i % 2 == 0){
+                System.out.println("Even: " + i);
+            }
+            else{
+                // System.out.println("Odd: " + i);
+            }
+        }
+    }
+}
