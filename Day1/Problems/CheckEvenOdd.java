9a58880a2e5c4e517db84c60252b3ec8fd5705a5
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-05 12:29:37 +0800

    Update CheckEvenOdd.java

--- Day1/Problems/CheckEvenOdd.java
+++ Day1/Problems/CheckEvenOdd.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = input.nextInt();
+
+        if(num % 2 == 0){
+            System.out.print(num + " is even");
+        }
+        else{
+            System.out.print(num + " is odd");
+        }
+
+    }
+}
