1e2e2f03be5e36dbd8bacf10b9425525f9faec4c
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-05 13:02:26 +0800

    Update SwapTwoNumbers.java

--- Day1/Problems/SwapTwoNumbers.java
+++ Day1/Problems/SwapTwoNumbers.java
@@ -1,1 +1,37 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter A: ");
+        int a = input.nextInt();
+        System.out.print("Enter B: ");
+        int b = input.nextInt();
+
+        System.out.println("Original Numbers:");
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+
+        /*
+
+        a = 10
+        b = 5
+
+        a = a + b = 15
+        b = a - b = 10
+        a = a - b = 5
+
+         */
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Swapped Numbers:");
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+        
+    }
+}
