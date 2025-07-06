371faf49360448b5579f8159c2d4bab710f23036
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-06 21:54:49 +0800

    Update IsAdultChecker.java

--- Day2/Problems/IsAdultChecker.java
+++ Day2/Problems/IsAdultChecker.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        int age = input.nextInt();
+
+        boolean isAdult;
+
+        if(age >= 18){
+            isAdult = true;
+        }
+        else{
+            isAdult = false;
+        }
+
+        System.out.println(isAdult);
+
+    }
+}
 
