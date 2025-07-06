35fc7944046d39c35cd914fbceb7b015f12ead31
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-06 22:09:32 +0800

    Update SimpleLogin.java

--- Day2/Problems/SimpleLogin.java
+++ Day2/Problems/SimpleLogin.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String hUsername = "Je1234";
+        String hPw = "Je1234";
+
+        System.out.print("Enter username: ");
+        String eUsername = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String ePw = input.nextLine();
+
+        if(hUsername.equals(eUsername) && hPw.equals(ePw)){
+            System.out.println("Login Successful");
+        }
+        else{
+            System.out.println("Wrong credentials");
+        }
+        
+    }
+}
 
