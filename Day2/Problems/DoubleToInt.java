a466f69bf4096612f69115f1d1049956ada57dec
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-06 21:51:41 +0800

    Update DoubleToInt.java

--- Day2/Problems/DoubleToInt.java
+++ Day2/Problems/DoubleToInt.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        double num = input.nextDouble();
+
+        int conum = (int) num;
+
+        System.out.print(conum);
+        
+    }
+}
 
