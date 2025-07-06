db62cb62938e3ad89551a70adfb622bc361efa2b
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-07-06 21:56:15 +0800

    Update FirstInitialChar.java

--- Day2/Problems/FirstInitialChar.java
+++ Day2/Problems/FirstInitialChar.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        
+        char initial = name.charAt(0);
+
+        System.out.println("Your first initial is: " + initial);
+    }
+}
 
